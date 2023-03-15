a90d4bfee08c19ea34eefbe86f542a4d17aad87e
DixonMch <dixonmogaka@gmail.com>
2023-03-15 13:11:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code here
 function moveDodgerLeft() {
-    var leftNumbers = dodger.style.left.replace("px", "");
-    var left = parseInt(leftNumbers, 10);
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
   
     if (left > 0) {
       dodger.style.left = `${left - 1}px`;
